4e556f34656801ae007c2a4e0f3d675aafe609b2
Sethios <sethiosmservice@outlook.fr>
2024-08-02 13:31:18 +0200

    navigate dom

--- v1/main.js
+++ v1/main.js
@@ -1,1 +1,23 @@
-import './style.css'
+import "./style.css";
+
+const app = document.querySelector("#app");
+
+// dynamization of title color and color text
+
+// const title_color = ["red", "blue", "yellow", "green"];
+
+// let i = 0;
+// setInterval(() => {
+//   app.style.background = title_color[i];
+//   i++;
+//   if (i > title_color.length - 1) {
+//     if (title_color[i] != "white") {
+//       app.style.color = "white";
+//       if (title_color[i] === "yellow" || title_color[i] === "white") {
+//         app.style.color = "dark";
+//       }
+//     }
+
+//     i = 0;
+//   }
+// }, 1500);

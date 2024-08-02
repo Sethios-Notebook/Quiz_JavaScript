0d4d9154a6d6265499e3d87a65401eaa75f3a094
Sethios <sethiosmservice@outlook.fr>
2024-08-02 15:28:10 +0200

    finish quiz detail

--- v1/main.js
+++ v1/main.js
@@ -1,82 +1,195 @@
-import { Questions } from "./questions";
 import "./style.css";
+import { Questions } from "./questions";
+
+const TIMEOUT = 4000;
 
 const app = document.querySelector("#app");
 
-const start_button = document.querySelector("#start");
+const startButton = document.querySelector("#start");
 
-start_button.addEventListener("click", start_quiz);
+startButton.addEventListener("click", startQuiz);
 
-function start_quiz(event) {
+function startQuiz(event) {
   event.stopPropagation();
+  let currentQuestion = 0;
+  let score = 0;
 
-  let current_question = 0;
-  let user_score = 0;
+  displayQuestion(currentQuestion);
 
-  cleanner_app();
-
-  function cleanner_app() {
+  function clean() {
     while (app.firstElementChild) {
       app.firstElementChild.remove();
     }
+    const progress = getProgressBar(Questions.length, currentQuestion);
+    app.appendChild(progress);
   }
 
-  dispaly_question(current_question);
-
-  function dispaly_question(index) {
+  function displayQuestion(index) {
+    clean();
     const question = Questions[index];
 
     if (!question) {
-      // finish quiz
+      displayFinishMessage();
+      return;
     }
 
-    const title = get_title_element(question.question);
+    const title = getTitleElement(question.question);
     app.appendChild(title);
+    const answersDiv = createAnswers(question.answers);
+    app.appendChild(answersDiv);
 
-    const answers_div = create_answer(question.answers);
-    app.appendChild(answers_div);
+    const submitButton = getSubmitButton();
 
-    const submit_button = get_submit_button();
-    app.appendChild(submit_button);
+    submitButton.addEventListener("click", submit);
+
+    app.appendChild(submitButton);
   }
 
-  function create_answer(answers) {
-    const answers_div = document.createElement("div");
+  function displayFinishMessage() {
+    const h1 = document.createElement("h1");
+    h1.innerText = "Bravo ! Tu as terminé le quiz.";
+    const p = document.createElement("p");
+    p.innerText = `Tu as eu ${score} sur ${Questions.length} point !`;
 
-    answers_div.classList.add("answers");
+    app.appendChild(h1);
+    app.appendChild(p);
+  }
 
-    for (const answer of answers) {
-      const label = get_answer_element(answer);
-      answers_div.appendChild(label);
+  function submit() {
+    const selectedAnswer = app.querySelector('input[name="answer"]:checked');
+
+    disableAllAnswers();
+
+    const value = selectedAnswer.value;
+
+    const question = Questions[currentQuestion];
+
+    const isCorrect = question.correct === value;
+
+    if (isCorrect) {
+      score++;
     }
 
-    return answers_div;
+    showFeedback(isCorrect, question.correct, value);
+    displayNextQuestionButton(() => {
+      currentQuestion++;
+      displayQuestion(currentQuestion);
+    });
+
+    const feedback = getFeedbackMessage(isCorrect, question.correct);
+    app.appendChild(feedback);
+  }
+
+  function createAnswers(answers) {
+    const answersDiv = document.createElement("div");
+
+    answersDiv.classList.add("answers");
+
+    for (const answer of answers) {
+      const label = getAnswerElement(answer);
+      answersDiv.appendChild(label);
+    }
+
+    return answersDiv;
   }
 }
 
-function get_title_element(text) {
+function getTitleElement(text) {
   const title = document.createElement("h3");
   title.innerText = text;
-
   return title;
 }
 
-function get_answer_element(text) {
-  const response = document.createElement("label");
-  response.innerText = text;
+function formatId(text) {
+  return text.replaceAll(" ", "-").replaceAll('"', "'").toLowerCase();
+}
+
+function getAnswerElement(text) {
+  const label = document.createElement("label");
+  label.innerText = text;
   const input = document.createElement("input");
-  const id = text.replaceAll(" ", "-").toLowerCase();
+  const id = formatId(text);
   input.id = id;
-  response.id = id;
+  label.htmlFor = id;
   input.setAttribute("type", "radio");
   input.setAttribute("name", "answer");
   input.setAttribute("value", text);
-  return response;
+  label.appendChild(input);
+  return label;
 }
 
-function get_submit_button() {
-  const submit_button = document.createElement("button");
-  submit_button.innerText = "Submit";
+function getSubmitButton() {
+  const submitButton = document.createElement("button");
+  submitButton.innerText = "Submit";
+  return submitButton;
+}
 
-  return submit_button;
+function showFeedback(isCorrect, correct, answer) {
+  const correctAnswerId = formatId(correct);
+  const correctElement = document.querySelector(
+    `label[for="${correctAnswerId}"]`
+  );
+
+  const selectedAnswerId = formatId(answer);
+  const selectedElement = document.querySelector(
+    `label[for="${selectedAnswerId}"]`
+  );
+
+  correctElement.classList.add("correct");
+  selectedElement.classList.add(isCorrect ? "correct" : "incorrect");
+}
+
+function getFeedbackMessage(isCorrect, correct) {
+  const paragraph = document.createElement("p");
+  paragraph.innerText = isCorrect
+    ? "Bravo ! Tu as eu la bonne réponse"
+    : `Désolé... mais la bonne réponse était ${correct}`;
+
+  return paragraph;
+}
+
+function getProgressBar(max, value) {
+  const progress = document.createElement("progress");
+  progress.setAttribute("max", max);
+  progress.setAttribute("value", value);
+  return progress;
+}
+
+function displayNextQuestionButton(callback) {
+  let remainingTimeout = TIMEOUT;
+
+  app.querySelector("button").remove();
+
+  const getButtonText = () => `Next (${remainingTimeout / 1000}s)`;
+
+  const nextButton = document.createElement("button");
+  nextButton.innerText = getButtonText();
+  app.appendChild(nextButton);
+
+  const interval = setInterval(() => {
+    remainingTimeout -= 1000;
+    nextButton.innerText = getButtonText();
+  }, 1000);
+
+  const timeout = setTimeout(() => {
+    handleNextQuestion();
+  }, TIMEOUT);
+
+  const handleNextQuestion = () => {
+    clearInterval(interval);
+    clearTimeout(timeout);
+    callback();
+  };
+
+  nextButton.addEventListener("click", () => {
+    handleNextQuestion();
+  });
+}
+
+function disableAllAnswers() {
+  const radioInputs = document.querySelectorAll('input[type="radio"]');
+
+  for (const radio of radioInputs) {
+    radio.disabled = true;
+  }
 }

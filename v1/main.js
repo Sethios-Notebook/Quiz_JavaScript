8a7abdd58cd994487151681d02b9c4fef6ae0e09
Sethios <sethiosmservice@outlook.fr>
2024-08-02 15:21:50 +0200

    question and answer map

--- v1/main.js
+++ v1/main.js
@@ -1,31 +1,57 @@
 import { Questions } from "./questions";
 import "./style.css";
 
+const app = document.querySelector("#app");
 
-const app = document.querySelector('#app');
+const start_button = document.querySelector("#start");
 
-const start_button = document.querySelector('#start');
+start_button.addEventListener("click", start_quiz);
 
-start_button.addEventListener("click", start_quiz)
+function start_quiz(event) {
+  event.stopPropagation();
 
-function start_quiz(event){
   let current_question = 0;
   let user_score = 0;
 
-  dispaly_question (current_question)
+  cleanner_app();
 
-  function dispaly_question(index){
-    const  question = Questions[index];
+  function cleanner_app() {
+    while (app.firstElementChild) {
+      app.firstElementChild.remove();
+    }
+  }
 
-    if(!question){
+  dispaly_question(current_question);
+
+  function dispaly_question(index) {
+    const question = Questions[index];
+
+    if (!question) {
       // finish quiz
     }
 
-
-const title = get_title_element(question.question)
+    const title = get_title_element(question.question);
     app.appendChild(title);
+
+    const answers_div = create_answer(question.answers);
+    app.appendChild(answers_div);
+
+    const submit_button = get_submit_button();
+    app.appendChild(submit_button);
   }
 
+  function create_answer(answers) {
+    const answers_div = document.createElement("div");
+
+    answers_div.classList.add("answers");
+
+    for (const answer of answers) {
+      const label = get_answer_element(answer);
+      answers_div.appendChild(label);
+    }
+
+    return answers_div;
+  }
 }
 
 function get_title_element(text) {
@@ -33,4 +59,24 @@ function get_title_element(text) {
   title.innerText = text;
 
   return title;
+}
+
+function get_answer_element(text) {
+  const response = document.createElement("label");
+  response.innerText = text;
+  const input = document.createElement("input");
+  const id = text.replaceAll(" ", "-").toLowerCase();
+  input.id = id;
+  response.id = id;
+  input.setAttribute("type", "radio");
+  input.setAttribute("name", "answer");
+  input.setAttribute("value", text);
+  return response;
+}
+
+function get_submit_button() {
+  const submit_button = document.createElement("button");
+  submit_button.innerText = "Submit";
+
+  return submit_button;
 }
